b2cac25644c64daf09013a986ffc551e380fd535
Akarsh Shrivastava <ak.shri48@gmail.com>
2019-02-16 01:09:22 +0530

    Cleaned some unwanted code

--- calculator.php
+++ calculator.php
@@ -59,46 +59,6 @@ if(isset($_POST['present']) && isset($_POST['days']) && $_POST['days']>= 0 && $_
 			echo '<a href="javascript:history.back()"><h4>Click to Go Back</h4></a>';
 			echo '</div>';
 		}
-	}
-	elseif( !empty($attend_percent) && !empty($days2)){ //Deleted Code. Not in any use now.
-		// $attend_percent = test_input($_POST['attend_percent']);
-		if (!preg_match("/^[0-9.]*$/",$attend_percent)) {
-			echo '<div  class= "container-fluid text-center bg-1 top" style="background-color:black;">';
-			echo '<br><br><br>Please enter correct Attendance Percentage.<br><br><br>';
-			echo '</div>';
-		}else{
-			$y= 100 + $days2;
-			$z= ($attend_percent/$y)*100;
-			$z = sprintf('%0.2f', $z  );
-			$new_z = $attend_percent - $z;
-			$new_z = sprintf('%0.2f', $new_z  );
-			$you_can_miss = 100-(($attend_percent*75)/100);
-			$you_can_miss = number_format($you_can_miss,0);
-			if($attend_percent<=100){
-			
-				echo '<div  class= "container-fluid text-center bg-1 top" style="background-color:black;"><br><br><br>';
-				echo 'Current Attendance is '.$attend_percent;
-				echo '<br>Attendance you will miss '.$new_z;
-				echo '<br>Attendance after missing '.$days2.' lecture/s will be '.$z;
-				if($z>=75){
-					
-				//	echo '<br>Lectures you can miss are '.$you_can_miss;
-					echo '<br><br><br></div>';
-				}else{
-					
-					echo '<br><br><br></div>';
-
-				}
-				
-			}else{
-				echo '<div  class="container-fluid text-center bg-1 top" style="background-color:black;">';
-				echo '<br><br><br>Total Attendance should not be more than 100<br><br>';
-				echo '<a class="up-arrow" href="#home" data-toggle="tooltip" title="TO TOP">
-				<span class="glyphicon glyphicon-chevron-up"></span></a>';
-				echo '<br><br><br></div>';
-				echo '**For Reference Only!**';
-			}
-		}
 	
 	}elseif(!empty($_POST['present']) && !empty($_POST['total']) && $days=='0' && ($_POST['total'] >= $_POST['present'])){
 		
